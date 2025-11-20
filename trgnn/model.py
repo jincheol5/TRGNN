d03713b030459cae9c7049d6f5d9645afba42ff1
jincheol5 <oscar4293@gmail.com>
2025-11-20 22:49:55 +0900

    update

--- trgnn/model.py
+++ trgnn/model.py
@@ -233,7 +233,7 @@ class TRGAT(nn.Module):
     def __init__(self,node_dim,latent_dim): 
         super().__init__()
         self.time_encoder=TimeEncoder(time_dim=latent_dim)
-        self.attention=GraphAttention(node_dim=node_dim,latent_dim=latent_dim)
+        self.embedding=GraphAttention(node_dim=node_dim,latent_dim=latent_dim)
         self.linear=nn.Linear(in_features=latent_dim,out_features=1)
         self.latent_dim=latent_dim
     
